24063d6a816d51be2089aa566bc0c5b9fe1b8462
jwhitlow45 <jwhitlow45@gmail.com>
2021-03-13 15:22:44 -0800

    lab5:cleaned up alu header file

--- lab5-boothsSimulator/include/ALU.hpp
+++ lab5-boothsSimulator/include/ALU.hpp
@@ -1,3 +1,5 @@
+#pragma once
+#include <cstddef>
 #define ALU_BITS 16
 
 using namespace std;
@@ -5,47 +7,33 @@ using namespace std;
 class ALU
 {
 private:
-    bool A[ALU_BITS];            //a register for alu
-    bool B[ALU_BITS];            //b register for alu
-    bool carry;                  //carry bit for alu
-    bool overflow;               //overflow bit for alu
+    bool A[ALU_BITS];                       //a register for alu
+    bool B[ALU_BITS];                       //b register for alu
+    bool carry;                             //carry bit for alu
+    bool overflow;                          //overflow bit for alu
 
 public:
-    //--------Member modifications methods--------
-    //A getter
+    //--------Register methods--------
+    //register accessors
     bool *getA() { return A; }
-
-    //B getter
     bool *getB() { return B; }
-
-    //set input to arg
-    void setA(bool *);
-    //clear inputs to 0000 0000 0000 0000
-    void clearA();
-
-    //set input to arg
-    void setB(bool *);
-    //clear inputs to 0000 0000 0000 0000
-    void clearB();
-
-    //set carry
-    void setCarry() { carry = true; }
-    //clear carry
-    void clearCarry() { carry = false; }
-    //get carry
     bool getCarry() { return carry; }
-
-    //set overflow
-    void setOverflow() { overflow = 1; }
-    //clear overflow
-    void clearOverflow() { overflow = 0; }
-    //get overflow
     bool getOverflow() { return overflow; }
 
+    void setA(bool *);                      //set a to passed value
+    void setB(bool *);                      //set b to passed value
+    void setCarry() { carry = 1; }          //set carry to 1
+    void setOverflow() { overflow = 1; }    //set overflow to 1
+
+    void clearA();                          //clear a to 0's
+    void clearB();                          //clear b to 0's
+    void clearCarry() { carry = 0; }        //clear carry to 0
+    void clearOverflow() { overflow = 0; }  //clear overflow to 0
+
     //--------ALU Operations--------
-    bool *add();
-    bool *subtract();
-    void negate(bool arg[ALU_BITS]);
+    bool *add();                            //add values in a and b (a+b)
+    bool *subtract();                       //sub values in a and b (a-b)
+    void negate(bool arg[ALU_BITS]);        //flip all bits in passed value
 
     //constructor
     ALU();
--- lab5-boothsSimulator/src/ALU.cpp
+++ lab5-boothsSimulator/src/ALU.cpp
@@ -1,4 +1,3 @@
-#include <cstddef>
 #include "ALU.hpp"
 
 using namespace std;
