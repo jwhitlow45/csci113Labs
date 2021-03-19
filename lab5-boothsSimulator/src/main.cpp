b433c2e7d9bb44523dc975a4b23bd705d2fb442f
jwhitlow45 <jwhitlow45@gmail.com>
2021-03-19 12:02:20 -0700

    lab5:implemented ui

--- lab5-boothsSimulator/src/main.cpp
+++ lab5-boothsSimulator/src/main.cpp
@@ -4,20 +4,47 @@
 
 using namespace std;
 
-void printArr(bool *arr, size_t size);
-
-void test();
+bool *getUserBinary(string);         //get binary from user and return binary array
+void stringToBinary(string, bool *); //convert string to binary array
+void printArr(bool *, size_t);
 
 int main()
 {
-    bool arra[16] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    bool arrb[16] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    //Given binary values
+    //strings for binary values
+    string md0 = "0000000110101001";
+    string mq0 = "1111111100110101";
+    string md1 = "0000000000101001";
+    string mq1 = "1111111111110101";
+    string md2 = "1111111111110101";
+    string mq2 = "0000000000101001";
+
+    // Multiplier object and pointer to store result pointer
     Multiplier *myMult = new Multiplier();
     bool *result;
 
-    cout << "CYCLE\t\tMD\t\t\tAC\t\t\tMQ\t\t\t\t\tMQ1\n";
+    string sUserMD;
+    string sUserMQ;
+    bool userMD[16];
+    bool userMQ[16];
 
-    result = myMult->multiply(arra, arrb);
+    cout << "Booth's Multiplication Simulator\n--------------------------------\n";
+    cout << "Enter a 16-bit binary number for MD register: ";
+    getline(cin, sUserMD);
+    stringToBinary(sUserMD, userMD);
+
+    cout << "Enter a 16-bit binary number for MQ register: ";
+    getline(cin, sUserMQ);
+    stringToBinary(sUserMQ, userMQ);
+
+    cout << "\n   Emulating\n";
+    printArr(userMD, 16);
+    cout << "\t*\n";
+    printArr(userMQ, 16);
+
+    cout << "\nCYCLE\t\tMD\t\t\tAC\t\t\tMQ\t\t\t\t\tMQ1\n";
+
+    result = myMult->multiply(userMD, userMQ);
     printArr(result, 32);
 
     delete result;
@@ -26,6 +53,12 @@ int main()
     myMult = nullptr;
 
     return 0;
+}
+
+void stringToBinary(string inputStr, bool *storageArr)
+{
+    for (size_t i = 0; i < ALU_BITS; i++)
+        storageArr[i] = inputStr[i] - '0'; //convert num char to dec
 }
 
 void printArr(bool *arr, size_t size)
